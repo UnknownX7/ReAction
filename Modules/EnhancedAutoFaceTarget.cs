210aaf0793cef5181bf8e83221b935658d22871b
UnknownX7 <UnknownX7@users.noreply.github.com>
2023-03-09 05:07:11 -0600

    Improve Enhanced Auto Face Target

--- Modules/EnhancedAutoFaceTarget.cs
+++ Modules/EnhancedAutoFaceTarget.cs
@@ -5,33 +5,32 @@ namespace ReAction.Modules;
 
 public unsafe class EnhancedAutoFaceTarget : PluginModule
 {
-    // cmp byte ptr [r15+33h], 6 -> test byte ptr [r15+3Ah], 10
-    private static readonly AsmPatch enhancedAutoFaceTargetPatch = new("41 80 7F 33 06 75 1E 48 8D 0D", new byte[] { 0x41, 0xF6, 0x47, 0x3A, 0x10 });
+    private static readonly AsmPatch removeAutoFaceTargetPatch = new("41 80 7F 33 06 75 1E 48 8D 0D", new byte[] { 0x90, 0x90, 0x90, 0x90, 0x90, 0xEB, 0x1C });
     private static readonly AsmPatch removeAutoFaceGroundTargetPatch = new("41 80 7F 33 06 74 22 49 8D 8E", new byte[] { 0x90, 0x90, 0x90, 0x90, 0x90, 0xEB });
 
     public override bool ShouldEnable => ReAction.Config.EnableEnhancedAutoFaceTarget;
 
-    protected override bool Validate() => enhancedAutoFaceTargetPatch.IsValid;
+    protected override bool Validate() => removeAutoFaceTargetPatch.IsValid;
 
     protected override void Enable()
     {
-        enhancedAutoFaceTargetPatch.Disable();
+        removeAutoFaceTargetPatch.Disable();
         removeAutoFaceGroundTargetPatch.Enable();
         ActionStackManager.PostActionStack += PostActionStack;
     }
 
     protected override void Disable()
     {
-        enhancedAutoFaceTargetPatch.Disable();
+        removeAutoFaceTargetPatch.Disable();
         removeAutoFaceGroundTargetPatch.Disable();
         ActionStackManager.PostActionStack -= PostActionStack;
     }
 
     private static void PostActionStack(ActionManager* actionManager, uint actionType, uint actionID, uint adjustedActionID, ref long targetObjectID, uint param, uint useType, int pvp)
     {
-        if (DalamudApi.DataManager.GetExcelSheet<Action>()?.GetRow(adjustedActionID) is not { Unknown50: 6 }) // These actions avoid facing the target by default, but some of them fail the patched check
-            enhancedAutoFaceTargetPatch.Enable();
+        if (DalamudApi.DataManager.GetExcelSheet<Action>()?.GetRow(adjustedActionID) is { Unknown26: false }) // This is checked by Client::Game::ActionManager_GetActionInRangeOrLoS
+            removeAutoFaceTargetPatch.Enable();
         else
-            enhancedAutoFaceTargetPatch.Disable();
+            removeAutoFaceTargetPatch.Disable();
     }
 }
